0dcb556005e036670126e325e228336393f4a4bf
Michael Gabriel A. Del Rosario <92383206+Subprograms@users.noreply.github.com>
2024-09-27 13:43:54 +0800

    Minor changes
    Printing commands upon exiting nested and unnested screens. Appended a default return value for a string function.

--- Homework 3 - Console Layout/boilerPlate.cpp
+++ Homework 3 - Console Layout/boilerPlate.cpp
@@ -29,20 +29,13 @@ std::string getCurrentTimestamp() {
 
 void printHeader() {
     std::cout << R"(
-    *************************************************************************************
-    *    _______    _______     ________  _________   __________   _______  __      __  *
-    *   //======\  //======\   //======\\ \=======\\  \=========  //======\  %\    //   *
-    *  //*         []          11      11  [[      ||  11         []          %\  //    *
-    *  []*         \\_______   [[      11  [[      ||  11######   \\_______    %\//     *
-    *  []*          ^######\\  [[      11  11######7   11          ^######\\    11      *
-    *  \&\                 //  11      11  #1          11_______          //    #1      *
-    *   \&#####%7  \%######7   \&######7/  ##         /#########  \%######7     ##      *
-    *___________________________________________________________________________________*
-    *************************************************************************************
-    Welcome to the Serial OS!
-    By: CSOPESY_S16 - Group 5
-
-    Type 'exit' to terminate, 'clear' to clear the terminal
+ _______  _______  _______  _______  _______  _______  __   __ 
+|       ||       ||       ||       ||       ||       ||  | |  |
+|       ||  _____||   _   ||    _  ||    ___||  _____||  |_|  |
+|       || |_____ |  | |  ||   |_| ||   |___ | |_____ |       |
+|      _||_____  ||  |_|  ||    ___||    ___||_____  ||_     _|
+|     |_  _____| ||       ||   |    |   |___  _____| |  |   |  
+|_______||_______||_______||___|    |_______||_______|  |___| 
     )" << std::endl;
 }
 
@@ -54,17 +47,16 @@ void clearScreen(bool shouldPrintHeader = true) {
 }
 
 void showCommands() {
-    std::cout << R"(
-    Available Commands:
-      screen            - Starts a new window within the screen
-                        - -s <name>: It will start a new screen given a name
-                        - -r <name>: Will reattach a previous screen
-      scheduler-test    - Does something for scheduler-test
-      scheduler-stop    - Does something for scheduler-stop
-      report-util       - Does something for report-util
-      clear             - Does something for scheduler-test
-      exit              - exit 
-    )" << std::endl;
+    std::cout << "Available commands:\n"
+        << "  marquee      - Does something for marquee\n"
+        << "  screen       - Starts a new window within the screen\n"
+        << "               - -s <name>: It will start a new screen given a name\n"
+        << "               - -r <name>: Will reattach a previous screen\n"
+        << "  process-smi  - Does something for process-smi\n"
+        << "  nvidia-smi   - Does something for nvidia-smi\n"
+        << "  clear        - Clears the screen\n"
+        << "  exit         - Exits the program\n"
+        << "  ?            - Shows this list of commands\n";
 }
 
 void displayProcessScreen(const ProcessScreen& ps) {
@@ -112,6 +104,7 @@ std::string handleScreenCommand(const std::string& command) {
                 return "ScreenError";
             }
         }
+        return "ScreenError";
     }
     else {
         std::cout << "Invalid screen command format." << std::endl;
@@ -132,9 +125,7 @@ void screenLoop(const std::string& name, bool isNested = false) {
             active = false;
             insideScreen = false;
             clearScreen();
-            if (!isNested) {
-                showCommands();
-            }
+            showCommands();
         }
         else if (command == "screen" || command.rfind("screen ", 0) == 0) {
             std::string result = handleScreenCommand(command);
