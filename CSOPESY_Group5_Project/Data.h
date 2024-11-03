e3d650aa8def441d7a2aba72ec1e29f990a46807
Michael Gabriel A. Del Rosario <92383206+Subprograms@users.noreply.github.com>
2024-11-03 13:53:34 +0800

    Update Data.h

--- CSOPESY_Group5_Project/Data.h
+++ CSOPESY_Group5_Project/Data.h
@@ -9,21 +9,14 @@ class Data {
 
 public:
     Data() {}
-
     std::vector<ProcessInfo> processList;
-
     ProcessInfo& getProcess(const std::string& processName);
-
     void createProcess(const std::string& processName);
-
     const std::vector<ProcessInfo>& getProcessList() const {
         return processList;
     }
-
     void updateProcessLine(const std::string& processName, int line);
-
     void listAllProcess();
-
     std::string getTimestamp() const;
 };
 
