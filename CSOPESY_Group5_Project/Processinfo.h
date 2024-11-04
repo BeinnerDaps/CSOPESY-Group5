9f19a466b883a29e2dfdf7df66561e8c0a13dae9
Michael Gabriel A. Del Rosario <92383206+Subprograms@users.noreply.github.com>
2024-11-04 16:24:44 +0800

    Update Processinfo.h

--- CSOPESY_Group5_Project/Processinfo.h
+++ CSOPESY_Group5_Project/Processinfo.h
@@ -3,17 +3,20 @@
 
 #include <string>
 
-struct ProcessInfo {
-    std::string processName;
+class ProcessInfo {
+public:
     int processID;
+    std::string processName;
     int totalLine;
-    std::string timeStamp;
-    bool isFinished;
     int currentLine;
     int assignedCore;
+    std::string timeStamp;
+    bool isFinished;
+    int remainingQuantum;
 
-    ProcessInfo(int id, const std::string& name, int totalLines, const std::string& timestamp, bool finished = false)
-        : processID(id), processName(name), totalLine(totalLines), timeStamp(timestamp), isFinished(finished), currentLine(0), assignedCore(-1) {} // Initialize currentLine and assignedCore
+    ProcessInfo(int id, const std::string& name, int lines, const std::string& timestamp, bool finished = false)
+        : processID(id), processName(name), totalLine(lines), currentLine(0), assignedCore(-1),
+        timeStamp(timestamp), isFinished(finished), remainingQuantum(0) {}
 };
 
 #endif
